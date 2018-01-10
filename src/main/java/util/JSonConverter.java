2e4c0f6beacbf84870a393b0a8659ef2c6163bf6
thisisgit1996 <leo.baker@qa.com>
2018-01-10 12:26:08 +0000

    "Third Try"

---
+++ src/main/java/util/JSonConverter.java
@@ -0,0 +1,17 @@
+package util;
+
+import com.google.gson.Gson;
+
+public class JSonConverter {
+	private Gson gson;
+	
+	public JSonConverter() {
+		this.gson = new Gson();
+	}
+	public String getJSONForObject (Object obj) {
+		return gson.toJson(obj);
+	}
+	public <T> T getObjectForJSON(String jsonString, Class<T> clazz) {
+		return gson.fromJson(jsonString, clazz);
+	}
+}
